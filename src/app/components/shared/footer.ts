40b9ff26c26b9e9bf30bde7fba8777da18c524db
softvar <varun2902@gmail.com>
2016-05-14 14:51:11 +0530

    basic footer component

---
+++ src/app/components/shared/footer.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: 'app/components/shared/footer.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Footer {
+  constructor() {}
+  ngOnInit() {}
+}
