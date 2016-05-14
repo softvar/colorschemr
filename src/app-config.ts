ee434e498ebb2ddfae171c291c93c3307da5dab7
softvar <varun2902@gmail.com>
2016-05-14 14:08:42 +0530

    using location as PathLocationStrategy, no longer legacy  in paths

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,12 +1,20 @@
+import {
+LocationStrategy,
+PathLocationStrategy
+} from '@angular/common'; // PathLocationStrategy - no longer legacy `#` in paths
+
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { provide } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
+
+// Angular2 rc-1 router doesn't support things as in beta version, which is kinda useful
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 import { MainApp } from './app/main';
 
 bootstrap(MainApp, [
   HTTP_PROVIDERS,
-  ROUTER_PROVIDERS)
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: PathLocationStrategy})
 ])
 .catch(err => console.error(err));
