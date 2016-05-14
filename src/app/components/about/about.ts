309751040dc939d6c603841885eeed2961080a92
softvar <varun2902@gmail.com>
2016-05-14 16:40:18 +0530

    basic about component

---
+++ src/app/components/about/about.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: 'app/components/about/about.html',
+  styleUrls: ['app/components/about/about.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class About {
+
+  constructor() {}
+  ngOnInit() {}
+}
