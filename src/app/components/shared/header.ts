b6f6784a9e01a6811975d3cf084d2a8d2a8f1e16
softvar <varun2902@gmail.com>
2016-06-26 14:01:30 +0530

    minor fixes, mobile friendly texts, styling tweaks

--- src/app/components/shared/header.ts
+++ src/app/components/shared/header.ts
@@ -7,13 +7,30 @@ import { MDL } from '../shared/mdl';
   encapsulation: ViewEncapsulation.None,
   selector: 'app-header',
   templateUrl: 'app/components/shared/header.html',
-  styleUrls: [ 'app/components/shared/css/style.css' ],
+  styleUrls: [
+    'app/assets/styles/responsive.css',
+    'app/components/shared/css/style.css'
+    ],
   providers: [],
   directives: [ MDL, ROUTER_DIRECTIVES ],
   pipes: []
 })
 export class Header {
   @Input() isAboutPage;
-  constructor() {}
-  ngOnInit() {}
+  texts: any = {};
+  constructor () {
+    this.init();
+  };
+  init () {
+    this.updateText();
+  };
+  updateText () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+      .test(navigator.userAgent)) {
+      this.texts.changePalettes = 'Swipe to generate colors';
+    } else {
+      this.texts.changePalettes = `Hit Spacebar to generate palettes,
+      num keys to update strip color`;
+    }
+  };
 }
